2517612ffc53c4c4bdb0d00353a7796b57aca10a
Vinicius Silva <vini-andrade@hotmail.com.br>
2016-08-22 20:28:08 -0300

    Fixed Layout

--- app/src/main/java/com/example/viniciusandrade/opentok/MainActivity.java
+++ app/src/main/java/com/example/viniciusandrade/opentok/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.viniciusandrade.opentok;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.util.Log;
+import android.widget.RelativeLayout;
+
 import com.opentok.android.Publisher;
 import com.opentok.android.PublisherKit;
 import com.opentok.android.Session;
@@ -18,36 +27,33 @@ public class MainActivity extends AppCompatActivity implements Session.SessionLi
 
     public static final String API_KEY = "45633112";
     public static final String SESSION_ID = "2_MX40NTYzMzExMn5-MTQ3MDU4NTA4NTA0N35NeFAwNGdNazhvRVd1VE5ZL3ZJeUtlNDd-fg";
-    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTYzMzExMiZzaWc9ZGQ4OWVkZjEwODg0ZmI4YWQ1ZGI4OGNjMDZmMDk5OTlkYTE3MmMxMjpzZXNzaW9uX2lkPTJfTVg0ME5UWXpNekV4TW41LU1UUTNNRFU0TlRBNE5UQTBOMzVOZUZBd05HZE5hemh2UlZkMVZFNVpMM1pKZVV0bE5EZC1mZyZjcmVhdGVfdGltZT0xNDcwNTg1MjYxJm5vbmNlPTAuODg2NTc4OTUwMDk5NjQ3JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE0NzA2NzE2NTk=";
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTYzMzExMiZzaWc9MTlmMWZhMmU4NDI3NzY0ZTc2YTRmMTcxMTdlMGFlYzA0OGVkYjI0ZTpzZXNzaW9uX2lkPTJfTVg0ME5UWXpNekV4TW41LU1UUTNNRFU0TlRBNE5UQTBOMzVOZUZBd05HZE5hemh2UlZkMVZFNVpMM1pKZVV0bE5EZC1mZyZjcmVhdGVfdGltZT0xNDcxOTA1MTEwJm5vbmNlPTAuODUyOTczMTg5MjA0OTMxMyZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDc0NDk3MTA4";
     public static final String LOGTAG = "MainActivity.class.getName()";
 
-    private LinearLayout publisherView;
-    private LinearLayout.LayoutParams publisherParams;
-    private LinearLayout subscriberView;
-    private LinearLayout.LayoutParams subscriberParams;
+    private RelativeLayout publisherView;
+    private RelativeLayout.LayoutParams publisherParams;
+    private RelativeLayout subscriberView;
+    private RelativeLayout.LayoutParams subscriberParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
         LinearLayout parentLayout = new LinearLayout(this);
         setContentView(parentLayout);
 
-        subscriberView = new LinearLayout(this);
-        subscriberParams = new LinearLayout.LayoutParams
-                (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        subscriberParams.weight = 0.5f;
+        subscriberView = new RelativeLayout(this);
+        subscriberParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
         subscriberView.setLayoutParams(subscriberParams);
 
-        publisherView = new LinearLayout(this);
-        publisherParams = new LinearLayout.LayoutParams
-                (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        publisherParams.weight = 0.5f;
+        publisherView = new RelativeLayout(this);
+        publisherParams = new RelativeLayout.LayoutParams(350, 350);
+        publisherParams.addRule(RelativeLayout.CENTER_IN_PARENT);
         publisherView.setLayoutParams(publisherParams);
 
-        parentLayout.setWeightSum(1f);
-        parentLayout.addView(publisherView);
         parentLayout.addView(subscriberView);
+        subscriberView.addView(publisherView);
 
         Session session = new Session(MainActivity.this, API_KEY, SESSION_ID);
         session.setSessionListener(this);
